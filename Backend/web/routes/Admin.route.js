7756d7dfcd5adb01b247eb69c84e7987a6ad3556
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2024-12-16 13:00:38 +0700

    route admin update Course

--- Backend/web/routes/Admin.route.js
+++ Backend/web/routes/Admin.route.js
@@ -125,15 +125,6 @@ router.put("/:MentorId/Mentor", async (req, res) => {
   const { courseID } = req.body; // courseID should be an array of ObjectIds
 
   try {
-    // const courseIDs = courseID.map(id => new mongoose.Types.ObjectId(id));
-
-    // const validCourses = await Course.find({ _id: { $in: courseIDs } });
-
-    // if (validCourses.length !== courseID.length) {
-    //   return res.status(400).json({
-    //     message: "One or more courseIDs do not exist in the Course collection",
-    //   });
-    // }
 
     const courseIDs= await validateArrayOfIDs(Course,courseID,"Course")
 
@@ -153,6 +144,58 @@ router.put("/:MentorId/Mentor", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while updating the mentor" });
+  }
+});
+
+router.put("/:courseId/Course", async (req, res) => {
+  const {courseId}= req.params;
+  const { name, desc, MentorID, daftarKelas } = req.body;
+
+  // Joi validation schema
+  const schema = Joi.object({
+    name: Joi.string().optional(),
+    desc: Joi.string().optional().allow(""),
+    MentorID: Joi.array().items(Joi.string()).optional(),
+    daftarKelas: Joi.array().items(Joi.string()).optional(),
+  });
+
+  // Validate request body
+  const { error } = schema.validate({ name, desc, MentorID, daftarKelas });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  try {
+    // Validate daftarKelas if provided
+    let daftarCourse
+    if (daftarKelas) {
+      daftarCourse= await validateArrayOfIDs(AnakMagang,daftarKelas,"AnakMagang")
+    }
+    // Check if MentorID exists\
+    let daftarMentor
+    if (MentorID) {
+       daftarMentor= await validateArrayOfIDs(Mentor,MentorID,"Mentor")
+    }
+    // Create new course
+    const updateCourse= await Course.findByIdAndUpdate(
+      courseId,
+      {
+        ...(name && { namaCourse: name }), // Update name if provided
+        ...(desc && { Deskripsi: desc }), // Update description if provided
+        ...(daftarMentor && daftarMentor.length > 0 && { mentorID: daftarMentor }), // Update mentorID if provided and not empty
+        ...(daftarCourse && daftarCourse.length > 0 && { daftarKelas: daftarCourse }), // Update daftarKelas if provided and not empty
+      },
+      { new: true }
+    )
+
+    if (!updateCourse) {
+      return res.status(404).json({ error: "course not found" });
+    }
+
+    res.status(200).json(updateCourse);
+  } catch (err) {
+    console.error("Error creating course:", err);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
