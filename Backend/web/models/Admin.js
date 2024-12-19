f73244103a879ee219a6c545411a202f7a7060b0
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2024-12-19 13:13:25 +0700

    update schema admin

--- Backend/web/models/Admin.js
+++ Backend/web/models/Admin.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
   userID: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'userData' },
+  calenderPicture: {
+    fileName: { type: String, default: null },
+    filePath: { type: String, default: null },
+    fileType: { type: String, default: null },
+    uploadDate: { type: Date, default: null },
+},
 }, {
   collection: 'Admin',
   timestamps: true
