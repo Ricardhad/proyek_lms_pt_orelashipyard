78095a2f787a8525f86b47f512ae5e977f8c74b1
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2025-01-20 20:01:59 +0700

    i

--- Backend/web/index.js
+++ Backend/web/index.js
@@ -19,7 +19,8 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
